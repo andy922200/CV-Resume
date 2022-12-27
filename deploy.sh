be73c1b4a40a9c3adeab2567930d97dc0b32f3ab
andy922200-m1-company <andy922200@gmail.com>
2022-12-27 14:25:18 +0800

    update script

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,12 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/andy922200/CV-Resume.git master:gh-pages
+git push -f https://github.com/andy922200/CV-Resume.git gh-pages
 
 cd -
